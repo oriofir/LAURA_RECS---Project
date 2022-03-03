0ee7087ac042706738e53f229e8ea3bbfef16fdd
oriofir <97814056+oriofir@users.noreply.github.com>
2022-03-03 13:31:34 -0800

    made .env.local file with API key for last.fm

--- src/HomeCarousel.js
+++ src/HomeCarousel.js
@@ -11,32 +11,15 @@ function Home(props) {
           alt="First slide"
         />
         <Carousel.Caption>
+          <iframe
+            width="100%"
+            height="400"
+            scrolling="no"
+            frameborder="no"
+            src="https://w.soundcloud.com/player/?visual=true&url=https%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F1207900078&show_artwork=true"
+          ></iframe>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item interval={1000}>
-        <img
-          className="d-block w-100"
-          src="https://i1.sndcdn.com/artworks-E6cTuMBcBTbnlpCX-iPySHQ-t500x500.jpg"
-          alt="Second slide"
-        />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://i1.sndcdn.com/artworks-E6cTuMBcBTbnlpCX-iPySHQ-t500x500.jpg"
-          alt="Third slide"
-        />
-        <Carousel.Caption interval={1000}>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
