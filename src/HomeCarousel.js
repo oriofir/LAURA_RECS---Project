00487cdac4f15dfcc75b3218173681c5f81aa87f
oriofir <97814056+oriofir@users.noreply.github.com>
2022-03-03 23:31:16 -0800

    got API to register on the console

--- src/HomeCarousel.js
+++ src/HomeCarousel.js
@@ -1,26 +1,50 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Carousel } from "react-bootstrap";
 import data from "./data.json";
+import { Button } from "react-bootstrap";
 
 const HomeCarousel = () => {
+  const [stream, setStream] = useState(data[0].stream);
+
+  const [similarArtists, setSimilarArtists] = useState([]);
+  const handleStream = () => {
+    setStream(stream);
+  };
+
+  useEffect(() => {
+    const url = `https://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=${data[0].artist}&api_key=4cbbdf80e334145c9a6ae6b78ec01e37&format=json`;
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setSimilarArtists(data);
+      });
+  }, []);
+
   return (
-    <Carousel style={{ minHeight: "90vh" }}>
+    <Carousel interval={10000} style={{ minHeight: "90vh" }}>
       {data.map((element, index) => {
         return (
           <Carousel.Item key={index} style={{ maxHeight: "90vh" }}>
             <img
               className="d-block w-100"
               style={{
-                height: "90vh",
+                height: "100vh",
                 width: "100%",
                 objectFit: "cover",
                 overflow: "hidden",
               }}
               src={element.thumbnail_url}
-              alt={data.title}
+              alt={element.title}
             />
-            <Carousel.Caption>
-              <p>{data.title}</p>
+
+            <Carousel.Caption className="carouselCaption">
+              <p>{element.title}</p>
+              <Button variant="dark" onClick={handleStream}>
+                Preview
+              </Button>{" "}
+              <Button variant="dark">Similar Artists</Button>{" "}
             </Carousel.Caption>
           </Carousel.Item>
         );
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Releases from "./Releases";
 import About from "./About";
+import "./index.css";
 
 function Navigation(props) {
   return (
